59b004e3c0ec15496306c0aa17e19cdb444cdb13
Maurício Siqueira <mauriciovsiqueira@outlook.com.br>
2026-06-11 13:53:47 -0300

    Nova aula

---
+++ dart/01_fundamentos/05_notacao_ponto.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Notação de ponto é uma forma de acessar membros (propriedades e métodos) de um objeto em Dart. 
+  // Ela é representada pelo operador "." (ponto) e é usada para acessar os membros de uma classe ou de um objeto específico.
+
+  // Exemplo com String
+  String nome = "Dart";
+  // Acessando a propriedade 'length' da string
+  print(nome.length); 
+  // Acessando o método 'toUpperCase()' da string
+  print(nome.toUpperCase()); 
+
+  // Exemplo com List
+  List<int> numeros = [1, 2, 3, 4, 5];
+  // Acessando a propriedade 'length' da lista
+  print(numeros.length); 
+  // Acessando o método 'isEmpty' da lista
+  print(numeros.isEmpty); 
+  
+
+}
